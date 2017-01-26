747125f6d8a5fd4d596fb98991db3e8e4b057f7d
James Marlowe <james.marlowe@lumate.com>
2017-01-25 23:01:47 -0600

    use real redirect url

--- assets/js/utils/AuthService.js
+++ assets/js/utils/AuthService.js
@@ -6,7 +6,7 @@ export default class AuthService {
     // Configure Auth0
     this.lock = new Auth0Lock(clientId, domain, {
       auth: {
-        redirectUrl: 'http://localhost:3000/login',
+        redirectUrl: 'https://app.cemaritan.com/login',
         responseType: 'token'
       }
     })
