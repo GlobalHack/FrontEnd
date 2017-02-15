476b434e02d6a25e22ad528d299bc19b6aa6a4ae
Ehren Coker <mrcoker@gmail.com>
2017-02-14 21:35:16 -0600

    allow boolean to be undefined

--- assets/js/components/AltBooleanInput.jsx
+++ assets/js/components/AltBooleanInput.jsx
@@ -4,54 +4,70 @@ import { connect } from 'react-redux'
 require('styles/components/AltBooleanInput')
 
 class BooleanInput extends Component {
-  constructor(props){
-      super(props)
-      console.log( props )
-      this.state = {}
-      if (typeof props.value !== 'undefined') this.state.val = props.value
+    constructor(props) {
+        super(props)
+        console.log(props)
+        this.state = {}
+        if (typeof props.value !== 'undefined') this.state.val = props.value
 
-      this.setTrue = this.setTrue.bind(this);
-      this.setFalse = this.setFalse.bind(this);
-  }
-  
-  componentDidMount(){
-    //   this.props.onChange( this.state.val )
-  }
+        this.setTrue = this.setTrue.bind(this);
+        this.setFalse = this.setFalse.bind(this);
+    }
 
-  componentDidUpdate(){
-      this.props.onChange( this.state.val )
-  }
+    componentDidMount() {
+        //   this.props.onChange( this.state.val )
+    }
 
-  setTrue(ev) {
-      ev.preventDefault()
-      this.setState({
-          val: true
-      }, function(){
-          this.props.onChange( this.state.val )
-      })
-  }
+    componentDidUpdate() {
+        this.props.onChange(this.state.val)
+    }
 
-  setFalse(ev) {
-      ev.preventDefault()
-      this.setState({
-          val: false
-      }, function(){
-          this.props.onChange( this.state.val )
-      })
-  }
+    setTrue(ev) {
+        ev.preventDefault()
+        if (this.state.val === true) {
+            this.setState({
+                val: undefined
+            }, function(){
+                this.props.onChange( undefined )
+            })
+        } else {
+            this.setState({
+                val: true
+            }, function () {
+                this.props.onChange(this.state.val)
+            })
+        }
+    }
+
+    setFalse(ev) {
+        ev.preventDefault()
+        if (this.state.val === false) {
+            this.setState({
+                val: undefined
+            }, function () {
+                this.props.onChange(undefined)
+            })
+        } else {
+            this.setState({
+                val: false
+            }, function () {
+                this.props.onChange(this.state.val)
+            })
+        }
+    }
 
 
-  render() {
-    return (
-        <div className="alt-boolean-input">
-            <input type="checkbox" id={ this.props.id } name={ this.props.id } checked={ this.state.val } />
-            <div className="alt-boolean-toggles">
-                <span className={`boolean boolean-true ${ this.state.val === true ? 'active' : '' }`} onClick={ this.setTrue }>Yes</span>
-                <span className={`boolean boolean-false ${ this.state.val === false ? 'active' : '' }`} onClick={ this.setFalse }>No</span>
+    render() {
+        return (
+            <div className="alt-boolean-input">
+                <input type="checkbox" id={this.props.id} name={this.props.id} checked={this.state.val === true} />
+                <div className="alt-boolean-toggles">
+                    <span className={`boolean boolean-true ${this.state.val === true ? 'active' : ''}`} onClick={this.setTrue}>Yes</span>
+                    <span className={`boolean boolean-false ${this.state.val === false ? 'active' : ''}`} onClick={this.setFalse}>No</span>
+                </div>
             </div>
-        </div>
-    );
-  }
+        );
+    }
 }
 BooleanInput.defaultProps = { incrementAmount: 1 }
 export default BooleanInput
