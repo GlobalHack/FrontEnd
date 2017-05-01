b2799a557935d637d719a9d335ab02c16181e022
Daniel Borstelmann <dborstelmann@wustl.edu>
2017-05-01 12:07:52 -0500

    iframe added

---
+++ src/components/referrals/ReferralPage.js
@@ -0,0 +1,32 @@
+import PropTypes from 'prop-types';
+import React from 'react';
+import AuthService from '../../utils/AuthService';
+
+export class Referral extends React.Component {
+  static contextTypes = {
+    router: PropTypes.object
+  };
+
+  static propTypes = {
+    location: PropTypes.object,
+    // auth: PropTypes.instanceOf(AuthService)
+  };
+
+  componentWillUnmount() {
+    // this.props.route.auth.lock.hide();
+  }
+
+  componentDidMount() {
+    // this.props.route.auth.lock.show({initialScreen: this.props.route.initialScreen || 'login'});
+  }
+
+  render() {
+    return (
+      <div className="jumbotron vertical-center">
+          <iframe className="referrals" src="http://referrals.ngrok.io" width="600px" height="600px" />
+      </div>
+    );
+  }
+}
+
+export default Referral;
