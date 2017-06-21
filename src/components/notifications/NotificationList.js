9038b5fcefcd7968c57154c811872469d72bee8d
James Marlowe <jameskmarlowe@gmail.com>
2017-06-21 00:17:03 -0500

    notification badge

--- src/components/notifications/NotificationList.js
+++ src/components/notifications/NotificationList.js
@@ -8,6 +8,10 @@ import MenuItem from 'material-ui/MenuItem';
 import {white} from 'material-ui/styles/colors';
 import IconButton from 'material-ui/IconButton';
 import Mail from 'material-ui/svg-icons/content/mail'
+import Delete from 'material-ui/svg-icons/action/delete';
+import ArrowDropRight from 'material-ui/svg-icons/navigation-arrow-drop-right';
+import RemoveRedEye from 'material-ui/svg-icons/image/remove-red-eye';
+import Badge from 'material-ui/Badge';
 
 class NotificationList extends React.Component {
   constructor(props, context) {
@@ -30,12 +34,21 @@ class NotificationList extends React.Component {
     return (
       <IconMenu
         color={white}
-        iconButtonElement={<IconButton><Mail color={white}/></IconButton>}
+        iconButtonElement={
+          <IconButton>
+            <Badge badgeContent={notifications.length} secondary={true} className='menu-badge' badgeStyle={{height:16,width:16}}>
+              <Mail color={white} />
+            </Badge>
+          </IconButton>
+        }
         targetOrigin={{horizontal: "right", vertical: "top"}}
         anchorOrigin={{horizontal: "right", vertical: "bottom"}}
       >
         {notifications.map(notification =>
-          <MenuItem key={1} primaryText={notification.text}/>
+          <MenuItem key={1} primaryText={notification.text} rightIcon={<ArrowDropRight />} menuItems={[
+            <MenuItem primaryText="View" leftIcon={<RemoveRedEye />} />,
+            <MenuItem primaryText="Remove" leftIcon={<Delete />} />
+          ]}/>
         )}
       </IconMenu>
     );
